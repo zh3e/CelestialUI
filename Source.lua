11097b31f953987b9768c440fdbce567c5ea8edf
Celestial <patrykwiatr48@gmail.com>
2026-01-18 17:39:29 +0000

    Update Source.lua

--- Source.lua
+++ Source.lua
@@ -521,7 +521,7 @@ function Library:create_ui()
     Icon.ScaleType = Enum.ScaleType.Fit
     Icon.BorderColor3 = Color3.fromRGB(0, 0, 0)
     Icon.AnchorPoint = Vector2.new(0, 0.5)
-    Icon.Image = 'rbxassetid://113979478438015'
+    Icon.Image = 'rbxassetid://10709819149'
     Icon.BackgroundTransparency = 1
     Icon.Position = UDim2.new(0.02500000037252903, 0, 0.054999999701976776, 0)
     Icon.Name = 'Icon'
